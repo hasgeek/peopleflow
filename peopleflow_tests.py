ffa0191a9f49926d36feb48b404eb239e32c38af
Deepak Jois <deepak.jois@gmail.com>
2012-07-04 11:32:17 +0530

    Travis CI integration
    * Added a no-op test script
    * Added travis configuration
    * Added a testing.py settings file

---
+++ peopleflow_tests.py
@@ -0,0 +1,19 @@
+import os
+import unittest
+from peopleflow import app
+
+
+class FlaskrTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.db_fd, app.config['DATABASE'] = os.tempfile.mkstemp()
+        app.config['TESTING'] = True
+        self.app = app.test_client()
+
+    def tearDown(self):
+        os.close(self.db_fd)
+        os.unlink(app.config['DATABASE'])
+
+if __name__ == '__main__':
+    os.environ['PEOPLEFLOW_ENV'] = 'test'
+    unittest.main()
