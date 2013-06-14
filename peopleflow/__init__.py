bb9ec603ebc636f5551163381e79712b96369cd9
Mitesh Ashar <email@miteshashar.com>
2013-06-14 21:46:15 +0530

    jQueryUI was needed for the autocomplete functionality used in the participants list page of an event.

--- peopleflow/__init__.py
+++ peopleflow/__init__.py
@@ -20,12 +20,14 @@ app = Flask(__name__, instance_relative_config=True)
 lastuser = Lastuser()
 
 assets['peopleflow.css'][version] = 'css/app.css'
+assets['jquery-ui.js']['1.8.4'] = 'js/libs/jquery-ui-1.8.4.min.js'
+assets['jquery-ui.css']['1.8.4'] = 'css/jquery-ui.css'
 
 from . import models, views
 from .models import db
 
 def init_for(env):
     coaster.app.init_app(app, env)
-    baseframe.init_app(app, requires=['baseframe', 'peopleflow'])
+    baseframe.init_app(app, requires=['baseframe', 'jquery-ui', 'peopleflow'])
     lastuser.init_app(app)
     lastuser.init_usermanager(UserManager(db, models.User))
