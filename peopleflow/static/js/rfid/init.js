1ea63ed032503bab0b6e19a4d37bc4a9a45a4707
Mitesh Ashar <email@miteshashar.com>
2013-06-18 09:00:54 +0530

    - Further generalisation of rfid/init.js. Closure'd - Sponsor Kiosk now uses the simplified rfid.on(action, fn) function.

--- peopleflow/static/js/rfid/init.js
+++ peopleflow/static/js/rfid/init.js
@@ -1,34 +1,48 @@
-var rfid = {
-    'init': function() {
-        for( action_name in this.indicator ) {
-            rfid[action_name] = function(data) {
-                this.action(action_name, data);
-            };
-        }
-        $(window).on('rfid:action', function(e, data) {
-            if( typeof rfid[data.action] == 'function' ) rfid[data.action](data);
-        });
+var rfid = function() {
+    var rfid_obj = {
+        'init': function() {
+            for( action_name in this.indicator ) {
+                this.actions[action_name] = [];
+                this.actions[action_name].push( function(data) {
+                    rfid_obj.indicator[action_name](data);
+                });
+            }
+            $(window).on('rfid:action', function(e, data) {
+                if( typeof rfid_obj.indicator[data.action] == 'function' ) {
+                    var actions = rfid_obj.actions[data.action];
+                    for( action in actions ) {
+                        if( typeof actions[action] == 'function' ) {
+                            actions[action](data);
+                        }
+                    }
+                }
+            });
 
-        $(window).on('rfid:server_inactive', function(e, data) {
-            $('#rfid_status').removeClass('server_active');
-        });
-        $(window).on('rfid:server_active', function(e, data) {
-            $('#rfid_status').addClass('server_active');
-        });
-        $('body').append('<a id="rfid_status" href="javascript:void(0)">&bull;</a>');
-    },
-    'action': function(action, data) {
-        if( typeof this.indicator[action] == 'function' ) {
-            this.indicator[action](data);
-        }
-    },
-    'indicator': {
-        'tag_placed': function(data) {
-            $('#rfid_status').addClass('tag_placed');
+            $(window).on('rfid:server_inactive', function(e, data) {
+                $('#rfid_status').removeClass('server_active');
+            });
+            $(window).on('rfid:server_active', function(e, data) {
+                $('#rfid_status').addClass('server_active');
+            });
+            $('body').append('<a id="rfid_status" href="javascript:void(0)">&bull;</a>');
         },
-        'tag_removed': function(data) {
-            $('#rfid_status').removeClass('tag_placed');
+        'actions': {},
+        'indicator': {
+            'tag_placed': function(data) {
+                $('#rfid_obj_status').addClass('tag_placed');
+            },
+            'tag_removed': function(data) {
+                $('#rfid_obj_status').removeClass('tag_placed');
+            }
+        },
+        'on': function(action_name, fn) {
+            if( typeof rfid_obj.indicator[action_name] == 'function' && typeof fn == 'function' ) {
+                rfid_obj.actions[action_name].push(fn);
+                return true;
+            }
+            else return false;
         }
-    }
-};
-rfid.init();
+    };
+    rfid_obj.init();
+    return {on: rfid_obj.on};
+}();
