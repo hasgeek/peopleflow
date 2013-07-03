d9b125ad1b4bde6b3d62365e0f8ffc25f154322a
Mitesh Ashar <email@miteshashar.com>
2013-07-03 17:18:40 +0530

    UI/UX for homepage. Added general boxed_list class that follows specific structure for showing a list of objects in well-formatted boxes.

--- peopleflow/views/index.py
+++ peopleflow/views/index.py
@@ -2,10 +2,11 @@
 # -*- coding: utf-8 -*-
 
 from .. import app
-from flask import render_template
+from .. import lastuser
 from ..models import Event
+from flask import render_template, g
 
 @app.route('/', methods=['GET'])
 def index():
     events = Event.query.order_by('from_date desc, to_date desc, date desc').all()
-    return render_template('index.html', events=events)
+    return render_template('index.html', events=events, siteadmin=lastuser.has_permission('siteadmin'), kioskadmin=lastuser.has_permission('kioskadmin'))
