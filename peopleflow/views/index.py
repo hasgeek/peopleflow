a254022f0a34a8177d62e9d67b8b9fa0225bba0d
Mitesh Ashar <email@miteshashar.com>
2013-07-03 08:51:23 +0530

    Order of events will be by date.

--- peopleflow/views/index.py
+++ peopleflow/views/index.py
@@ -7,5 +7,5 @@ from ..models import Event
 
 @app.route('/', methods=['GET'])
 def index():
-    events = Event.query.order_by('id').all()
+    events = Event.query.order_by('from_date desc, to_date desc, date desc').all()
     return render_template('index.html', events=events)
