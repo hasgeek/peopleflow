d68f0f0ff2d5dab85b8eed88855a7b2ef14ef547
Mitesh Ashar <email@miteshashar.com>
2013-07-08 21:20:24 +0530

    Participant columns changed

--- peopleflow/models/participant.py
+++ peopleflow/models/participant.py
@@ -25,18 +25,18 @@ class Participant(db.Model, BaseMixin):
     name = db.Column(db.Unicode(80), nullable=False)
     #: Email of the participant
     email = db.Column(db.Unicode(80), nullable=False)
-    #: Ticket type
-    ticket_type = db.Column(db.Unicode(80), default=u"Regular", nullable=True)
     #: Company name
     company = db.Column(db.Unicode(80), nullable=True)
     #: Job title
     job = db.Column(db.Unicode(80), nullable=True)
     #: City
     city = db.Column(db.Unicode(80), nullable=True)
+    #: Phone
+    phone = db.Column(db.Unicode(15), nullable=True)
     #: Twitter handle
     twitter = db.Column(db.Unicode(80), nullable=True)
-    #: T-shirt size
-    tshirt_size = db.Column(db.Integer, nullable=False, default=0)
+    #: Whether the participant has purchased a T-shirt
+    purchased_tee = db.Column(db.Boolean, default=False, nullable=True)
     #: Date of registration
     regdate = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
     #:NFC ID
@@ -47,6 +47,8 @@ class Participant(db.Model, BaseMixin):
     attended = db.Column(db.Boolean, default=False, nullable=False)
     #: Datetime the participant showed up
     attend_date = db.Column(db.DateTime, nullable=True)
+    #: Purchases made by the participant
+    purchases = db.Column(db.Unicode(200), nullable=True)
     #: Event the participant is attending
     event_id = db.Column(db.Integer, db.ForeignKey('event.id'))
 
