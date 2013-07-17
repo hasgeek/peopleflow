935f5b19904fd4576aa1cac8dd5b8f1436380c1d
Mitesh Ashar <email@miteshashar.com>
2013-07-18 00:30:07 +0530

    New Kiosk Form now uses render_form

--- peopleflow/views/kiosk.py
+++ peopleflow/views/kiosk.py
@@ -11,7 +11,7 @@ from .. import lastuser
 from ..models import db, Kiosk, Event, Participant, CXLog
 from ..forms import KioskForm, ConfirmSignoutForm
 from flask import request, flash, url_for, render_template, jsonify, make_response
-from baseframe.forms import render_redirect, ConfirmDeleteForm
+from baseframe.forms import render_redirect, ConfirmDeleteForm, render_form
 from coaster import make_name
 from coaster.views import jsonp, load_model, load_models
 from coaster.gfm import markdown
@@ -21,35 +21,27 @@ from flask.ext.mail import Mail, Message
 @app.route('/event/<id>/kiosk/new', methods=['GET', 'POST'])
 @lastuser.requires_permission('siteadmin')
 @load_model(Event, {'id':'id'}, 'event')
-def kiosk_new(event, kioskform=None):
-    if request.method == 'GET':
-        if kioskform is None:
-            kioskform = KioskForm()
-        context = {'kioskform':kioskform, 'event':event}
-        return render_template('new_kiosk.html', **context)
-
-    if request.method == 'POST':
-        form = KioskForm()
-        if form.validate_on_submit():
-            kiosk = Kiosk()
-            form.populate_obj(kiosk)
-            file = urllib.urlopen(kiosk.company_logo).read()
-            filename = hashlib.md5(file).hexdigest()
-            filepath = os.path.join(app.config['STATIC_UPLOAD_FOLDER'], 'sponsors', filename)
-            with open(filepath, 'wb') as f:
-                f.write(file)
-                f.close()
-            kiosk.company_logo = filename
-            db.session.add(kiosk)
+def kiosk_new(event):
+    form = KioskForm()
+    if form.validate_on_submit():
+        kiosk = Kiosk()
+        form.populate_obj(kiosk)
+        file = urllib.urlopen(kiosk.company_logo).read()
+        filename = hashlib.md5(file).hexdigest()
+        filepath = os.path.join(app.config['STATIC_UPLOAD_FOLDER'], 'sponsors', filename)
+        with open(filepath, 'wb') as f:
+            f.write(file)
+            f.close()
+        kiosk.company_logo = filename
+        kiosk.event_id = event.id
+        db.session.add(kiosk)
+        try:
             db.session.commit()
             flash("Kiosk added")
-            return render_redirect(url_for('index'), code=303)
-        else:
-            if request.is_xhr:
-                return render_template('kioskform.html', kioskform=form, ajax_re_register=True)
-            else:
-                flash("Please check your details and try again.", 'error')
-                return event_add(kioskform=form)
+            return render_redirect(url_for('event_kiosks', event=event.id))
+        except:
+            pass
+    return render_form(form=form, title=u"New Kiosk - " + event.title, submit=u"Add", cancel_url=url_for('event_kiosks', event=event.id))
 
 #TODO: Check for kiosk-event releationship when fetching kiosk
 @app.route('/event/<event>/kiosk/<kiosk>', methods=['GET','POST'])
