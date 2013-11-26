f826870e0bc5986e0d207156cee8d66b48429791
Mitesh Ashar <email@miteshashar.com>
2013-11-27 01:48:08 +0530

    Print CREW labels.

--- peopleflow/helpers/printlabel.py
+++ peopleflow/helpers/printlabel.py
@@ -17,7 +17,7 @@ def printlabel(printer, lines):
     fname = f.name
     f.close()
 
-    heights = [18, 32, 38]
+    heights = [18, 32, 38, 47]
 
     doc = SimpleDocTemplate(fname,
                             pagesize=(62 * mm, heights[len(lines) - 1] * mm),
@@ -27,14 +27,17 @@ def printlabel(printer, lines):
 
     styles = [
         ParagraphStyle("s1", fontName="Helvetica-Bold", alignment=TA_CENTER, fontSize=15, leading=15),
-        ParagraphStyle("s2", fontName="Helvetica", alignment=TA_CENTER, fontSize=15, leading=18, spaceBefore=4, textColor=u"#555555"),
-        ParagraphStyle("s3", fontName="Helvetica", alignment=TA_CENTER, fontSize=12, leading=13, spaceBefore=4, textColor=u"#777777")
+        ParagraphStyle("s2", fontName="Helvetica", alignment=TA_CENTER, fontSize=15, leading=18, spaceBefore=4),
+        ParagraphStyle("s3", fontName="Helvetica", alignment=TA_CENTER, fontSize=12, leading=13, spaceBefore=4),
+        ParagraphStyle("s4", fontName="Helvetica-Bold", alignment=TA_CENTER, fontSize=20, leading=22, textColor="#444444"),
         ]
     for i, line in enumerate(lines):
         if i < len(styles):
             story.append(Paragraph(line, styles[i]))
             if i == 0 and len(lines) > 1:
-                story.append(HRFlowable(width='95%', color=u"#333333", spaceBefore=5))
+                story.append(HRFlowable(width='95%', spaceBefore=5))
+    if lines[len(lines) - 1] == "CREW":
+        story = story[-1:] + story[:-1]
     doc.build(story)
 
     os.system("lpr -P %s %s" % (printer, fname))
@@ -50,6 +53,8 @@ def make_label_content(participant):
         data.append(compline)
     if(participant.twitter):
         data.append('@' + participant.twitter)
+    if u'Crew' in participant.purchases:
+        data.append("CREW")
     return data
 
 
@@ -58,6 +63,6 @@ if __name__ == '__main__':
     parser.add_argument('printer', type=str, help='The name of the printer')
     parser.add_argument('--lines', type=int, help='The number of lines to print', default=3)
     args = parser.parse_args()
-    data = ["Kiran Jonnalagadda isn't long enough", "CEO, HasGeek Media LLP.", "@jackerhack"]
+    data = ["Kiran Jonnalagadda isn't long enough", "CEO, HasGeek Media LLP.", "@jackerhack", "CREW"]
     data = data[:args.lines]
     printlabel(args.printer, data)
