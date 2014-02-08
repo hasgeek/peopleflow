83f70c4cf0085362853d9c0b35ff10b33544e161
Mitesh Ashar <email@miteshashar.com>
2014-02-08 23:14:18 +0530

    printlabel now supports printing of badges.

--- peopleflow/helpers/printlabel.py
+++ peopleflow/helpers/printlabel.py
@@ -11,26 +11,39 @@ from reportlab.lib.styles import ParagraphStyle
 from reportlab.lib.colors import Color
 
 
-def printlabel(printer, lines):
+def printlabel(printer, print_type, lines, options={}):
 
     f = tempfile.NamedTemporaryFile(suffix='.pdf', delete=False)
     fname = f.name
     f.close()
+    if print_type == 'label':
+        heights = [18, 32, 38, 47]
 
-    heights = [18, 32, 38, 47]
-
-    doc = SimpleDocTemplate(fname,
-                            pagesize=(62 * mm, heights[len(lines) - 1] * mm),
-                            topMargin=0, leftMargin=0,
-                            rightMargin=0, bottomMargin=0)
-    story = []
-
-    styles = [
+        doc = SimpleDocTemplate(fname,
+                                pagesize=(62 * mm, heights[len(lines) - 1] * mm),
+                                topMargin=0, leftMargin=0,
+                                rightMargin=0, bottomMargin=0)
+        styles = [
         ParagraphStyle("s1", fontName="Helvetica-Bold", alignment=TA_CENTER, fontSize=15, leading=15),
         ParagraphStyle("s2", fontName="Helvetica", alignment=TA_CENTER, fontSize=15, leading=18, spaceBefore=4),
         ParagraphStyle("s3", fontName="Helvetica", alignment=TA_CENTER, fontSize=12, leading=13, spaceBefore=4),
         ParagraphStyle("s4", fontName="Helvetica-Bold", alignment=TA_CENTER, fontSize=20, leading=22, textColor="#444444"),
         ]
+    else:
+        doc = SimpleDocTemplate(fname,
+                                pagesize=(95 * mm, 134 * mm),
+                                topMargin=(60 if 'topMargin' not in options else options['topMargin']) * mm,
+                                leftMargin=(1 if 'leftMargin' not in options else options['leftMargin']) * mm,
+                                rightMargin=(1 if 'rightMargin' not in options else options['rightMargin']) * mm,
+                                bottomMargin=(9 if 'bottomMargin' not in options else options['bottomMargin']) * mm)
+        styles = [
+        ParagraphStyle("s1", fontName="Helvetica-Bold", alignment=TA_CENTER, fontSize=20, leading=22, textColor="#DF5E0E"),
+        ParagraphStyle("s2", fontName="Helvetica", alignment=TA_CENTER, fontSize=17, leading=19, spaceBefore=2, textColor="#428BCA"),
+        ParagraphStyle("s3", fontName="Helvetica-Bold", alignment=TA_CENTER, fontSize=15, leading=18, spaceBefore=2, textColor="#55acee"),
+        ParagraphStyle("s4", fontName="Helvetica-Bold", alignment=TA_CENTER, fontSize=22, leading=30, textColor="#444444"),
+        ]
+    story = []
+
     for i, line in enumerate(lines):
         if i < len(styles):
             story.append(Paragraph(line, styles[i]))
@@ -62,7 +75,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Script to test printing of labels. Tries to test through fairly large 2-liners for name and job+company.')
     parser.add_argument('printer', type=str, help='The name of the printer')
     parser.add_argument('--lines', type=int, help='The number of lines to print', default=3)
+    parser.add_argument('--type', type=str, help='Print type. Either label or badge', default='label')
     args = parser.parse_args()
     data = ["Kiran Jonnalagadda isn't long enough", "CEO, HasGeek Media LLP.", "@jackerhack", "CREW"]
     data = data[:args.lines]
-    printlabel(args.printer, data)
+    printlabel(args.printer, args.type, data)
