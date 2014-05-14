cdf427552c26f0d243f596aba25eb24fd6f2c372
Mitesh Ashar <email@miteshashar.com>
2014-05-15 05:07:38 +0530

    Support for tinyMCE change in bootstrap. @jace TinyMce4Field throws a JS error: "Uncaught ReferenceError: tinymce is not defined "

--- peopleflow/forms/kiosk.py
+++ peopleflow/forms/kiosk.py
@@ -2,7 +2,7 @@
 
 import wtforms
 import wtforms.fields.html5
-from baseframe.forms import Form, RichTextField
+from baseframe.forms import Form, TinyMce3Field
 
 from ..models import Event
 
@@ -11,7 +11,7 @@ class KioskEditForm(Form):
     company = wtforms.TextField('Company Name', description="Optional. You may want to leave this attribute empty if the name of the company is clear from the logo.")
     company_tag = wtforms.TextField('Company Tagline', description="Optional")
     tap_msg = wtforms.TextField('Tapping message', description='Optional. Please tap your badge to ____? Leave blank to use default message.')
-    privacy_policy = RichTextField('Privacy Policy')
+    privacy_policy = TinyMce3Field('Privacy Policy')
 
 class KioskLogoForm(Form):
     company_logo = wtforms.TextField('Company Logo', description="Provide the URL for the image of the sponsor's logo.", validators=[wtforms.validators.Required('The logo is required'), wtforms.validators.URL(message="Please provide a valid URL")])
