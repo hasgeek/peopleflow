e08bc5d5117fb979f3ddda03675e54e5630639c7
Mitesh Ashar <email@miteshashar.com>
2014-07-24 03:53:47 +0530

    Product name is only controllable when the source of product is not an external sync.

--- peopleflow/forms/product.py
+++ peopleflow/forms/product.py
@@ -7,3 +7,8 @@ from coaster.utils import nullint
 class ProductForm(Form):
     title = wtforms.TextField('Product Name', validators=[wtforms.validators.Required('A name is required')])
     ticket_id = wtforms.fields.SelectField('Ticket', description=u'If this this a ticket, which one is it?', coerce=nullint, choices=[], default='')
+
+    def __init__(self, formdata=None, obj=None, prefix='', **kwargs):
+        super(ProductForm, self).__init__(formdata=formdata, obj=obj, prefix=prefix, **kwargs)
+        if obj and obj.source:
+            del self.title
